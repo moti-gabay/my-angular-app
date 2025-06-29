b8446dbf48c8197ecc5faee90377a83f9d69b5ba
moti gabay <motigabay18@gmail.com>
2025-06-29 17:38:10 +0300

    style siedebar

--- src/app/components/login-register/login-register.ts
+++ src/app/components/login-register/login-register.ts
@@ -10,7 +10,7 @@ import { take } from 'rxjs/operators'; // ייבוא take
   selector: 'app-login-register',
   // standalone: true,
   imports: [CommonModule, FormsModule],
-  templateUrl:"./login-register.html",
+  templateUrl: "./login-register.html",
   styleUrls: ['./login-register.css'] // <--- הוסף שורה זו
 
 })
@@ -46,16 +46,19 @@ export class LoginRegisterComponent {
           this.authService.currentUser$.pipe(take(1)).subscribe(user => {
             if (user) {
               if (user.role === 'admin' || user.role === 'member') {
+                this.resetUserData();
                 this.router.navigate(['/files']);
               } else if (user.role === 'user') {
+                this.resetUserData();
                 this.router.navigate(['/dashboard']);
               }
             } else {
+              this.resetUserData();
               this.router.navigate(['/login']); // למקרה שההתחברות הצליחה אבל לא קיבלנו משתמש
             }
           });
         },
-        error: (err : any) => {
+        error: (err: any) => {
           this.errorMessage = err.error?.message || 'שגיאה בהתחברות.';
           console.error('Login error:', err);
         }
@@ -67,7 +70,7 @@ export class LoginRegisterComponent {
           this.isLoginMode = true;
           this.resetUserData();
         },
-        error: (err : any) => {
+        error: (err: any) => {
           this.errorMessage = err.error?.message || 'שגיאה בהרשמה.';
           console.error('Register error:', err);
         }
