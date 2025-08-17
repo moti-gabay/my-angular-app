9c045702324d8cb4322699361d8f9f63c2c14488
moti gabay <motigabay18@gmail.com>
2025-08-18 01:44:20 +0300

    Lazy Loaded in app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,127 +1,102 @@
-// src/app/app.routes.ts
 import { Routes } from '@angular/router';
-import { LoginRegisterComponent } from './components/login-register/login-register';
-import { FileManagerComponent } from './file-manager/file-manager';
-import { UserDashboardComponent } from './components/user-dashbord/user-dashbord';
 import { roleGuard } from './auth/role.guard';
-import { HomePage } from './components/home-page/home-page';
-import { News } from './components/news/news';
-import { Tradition } from './components/tradition/tradition';
-import { AddEventComponent } from './components/add-event/add-event';
-import { ImageGalleryComponent } from './image-gallery/image-gallery';
-import { DonationComponent } from './components/donation/donation';
-import { ApprovedEventsComponent } from './components/approved-events/approved-events';
-import { EventListComponent } from './event-list/event-list';
-import { ContactComponent } from './components/contact/contact';
-import { NewsInfo } from './components/news-info/news-info';
-import { TraditionInfo } from './components/tradition-info/tradition-info';
-import { NewsEditComponent } from './components/news-edit/news-edit';
-import { NewsAddComponent } from './components/news-add/news-add';
-import { TraditionAddComponent } from './components/tradition-add/tradition-add';
-import { TraditionEditComponent } from './components/tradition-edit/tradition-edit';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginRegisterComponent },
+  { path: 'login', loadComponent: () => import('./components/login-register/login-register').then(m => m.LoginRegisterComponent) },
 
   {
     path: 'files',
-    component: FileManagerComponent,
+    loadComponent: () => import('./file-manager/file-manager').then(m => m.FileManagerComponent),
     canActivate: [roleGuard],
     data: { roles: ['admin', 'member'] }
   },
   {
     path: 'images',
-    component: ImageGalleryComponent,
+    loadComponent: () => import('./image-gallery/image-gallery').then(m => m.ImageGalleryComponent),
     canActivate: [roleGuard],
     data: { roles: ['admin', 'member'] }
   },
   {
-    path: 'admin-events',
-    component: EventListComponent,
-    canActivate: [roleGuard],
-    data: { roles: ['admin'] }
-  },
-  {
     path: 'dashboard',
-    component: UserDashboardComponent,
-    canActivate: [roleGuard],
-    data: { roles: ['user', 'admin', 'member'] }
-  },
-  {
-    path: 'homepage',
-    component: HomePage,
+    loadComponent: () => import('./components/user-dashbord/user-dashbord').then(m => m.UserDashboardComponent),
     canActivate: [roleGuard],
     data: { roles: ['user', 'admin', 'member'] }
   },
   {
     path: 'news',
-    component: News,
+    loadComponent: () => import('./components/news/news').then(m => m.News),
     canActivate: [roleGuard],
     data: { roles: ['user', 'admin', 'member'] }
   },
- {
-  path: 'edit-news/:id',
-  component: NewsEditComponent,
-  canActivate: [roleGuard],
-  data: { roles: ['admin'] }
- },
   {
-    path: 'add-news',
-    component: NewsAddComponent,
+    path: 'news/:id',
+    loadComponent: () => import('./components/news-info/news-info').then(m => m.NewsInfo),
+    canActivate: [roleGuard],
+    data: { roles: ['user', 'admin', 'member'] }
+  },
+  {
+    path: 'edit-news/:id',
+    loadComponent: () => import('./components/news-edit/news-edit').then(m => m.NewsEditComponent),
     canActivate: [roleGuard],
     data: { roles: ['admin'] }
   },
- {
-  path: 'news/:id',
-  component: NewsInfo,
-  canActivate: [roleGuard],
-  data: { roles: ['user', 'admin', 'member'] }
- },
+  {
+    path: 'add-news',
+    loadComponent: () => import('./components/news-add/news-add').then(m => m.NewsAddComponent),
+    canActivate: [roleGuard],
+    data: { roles: ['admin'] }
+  },
   {
     path: 'tradition',
-    component: Tradition,
+    loadComponent: () => import('./components/tradition/tradition').then(m => m.Tradition),
     canActivate: [roleGuard],
     data: { roles: ['user', 'admin', 'member'] }
+  },
+  {
+    path: 'tradition/:id',
+    loadComponent: () => import('./components/tradition-info/tradition-info').then(m => m.TraditionInfo),
+    canActivate: [roleGuard],
+    data: { roles: ['user', 'admin', 'member'] }
+  },
+  {
+    path: 'edit-tradition/:id',
+    loadComponent: () => import('./components/tradition-edit/tradition-edit').then(m => m.TraditionEditComponent),
+    canActivate: [roleGuard],
+    data: { roles: ['admin'] }
   },
   {
     path: 'add-tradition',
-    component: TraditionAddComponent,
+    loadComponent: () => import('./components/tradition-add/tradition-add').then(m => m.TraditionAddComponent),
     canActivate: [roleGuard],
     data: { roles: ['admin'] }
   },
- {
-  path: 'tradition/:id',
-  component: TraditionInfo,
-  canActivate: [roleGuard],
-  data: { roles: ['user', 'admin', 'member'] }
- },
- {
-  path: 'edit-tradition/:id',
-  component: TraditionEditComponent,
-  canActivate: [roleGuard],
-  data: { roles: ['admin'] }
- },
   {
     path: 'events',
-    component: ApprovedEventsComponent,
+    loadComponent: () => import('./components/approved-events/approved-events').then(m => m.ApprovedEventsComponent),
     canActivate: [roleGuard],
     data: { roles: ['user', 'admin', 'member'] }
   },
   {
+    path: 'admin-events',
+    loadComponent: () => import('./event-list/event-list').then(m => m.EventListComponent),
+    canActivate: [roleGuard],
+    data: { roles: ['admin'] }
+  },
+  {
     path: 'contact',
-    component: ContactComponent,
+    loadComponent: () => import('./components/contact/contact').then(m => m.ContactComponent),
     canActivate: [roleGuard],
     data: { roles: ['user', 'admin', 'member'] }
   },
   {
     path: 'add-event',
-    component: AddEventComponent,
+    loadComponent: () => import('./components/add-event/add-event').then(m => m.AddEventComponent),
     canActivate: [roleGuard],
     data: { roles: ['user', 'admin', 'member'] }
   },
   {
     path: 'donation',
-    component: DonationComponent,
+    loadComponent: () => import('./components/donation/donation').then(m => m.DonationComponent),
     canActivate: [roleGuard],
     data: { roles: ['user', 'admin', 'member'] }
   },
