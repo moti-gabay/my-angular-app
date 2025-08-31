8b2b69d0d531b33ed416b92ffeafe0a765c920b9
moti gabay <motigabay18@gmail.com>
2025-08-31 20:01:11 +0300

    fix new url

--- src/app/services/url.ts
+++ src/app/services/url.ts
@@ -1,3 +1,3 @@
 // export const API_URL = 'http://localhost:5000';
-export const API_URL = 'https://python-app-2-5i73.onrender.com';
+export const API_URL = 'https://python-app-3-5q6d.onrender.com';
 export const API = 'http://localhost:5000';
